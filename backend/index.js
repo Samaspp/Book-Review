af3270611b919b96ba27123895767f633bb3051a
sami <samiap914@gmail.com>
2025-06-19 21:43:00 +0530

    Use .env for MongoDB connection

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,19 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const connectDB = require('./config/db');
-
+const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 connectDB(); 
 
-const bookRoutes = require('./routes/bookRoutes');
-app.use('/api/books', bookRoutes);
-const reviewRoutes = require('./routes/reviewRoutes');
-app.use('/api/reviews', reviewRoutes);
-const userRoutes = require('./routes/userRoutes');
-app.use('/api/users', userRoutes);
+app.use('/api/books', require('./routes/bookRoutes'));
+app.use('/api/reviews', require('./routes/reviewRoutes'));
+app.use('/api/users', require('./routes/userRoutes'));
 
 
 app.get('/', (req, res) => res.send(' Book Review API Running'));
 
-app.listen(5000, () => console.log(' Server running on http://localhost:5000'));
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
